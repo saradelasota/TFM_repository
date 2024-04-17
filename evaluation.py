5beb6c5618928b3fe3a126c68dfd42506162e13b
saradelasota <sara.delasota@gmail.com>
2024-04-17 22:31:06 +0200

    evaluation_function

--- evaluation.py
+++ evaluation.py
@@ -9,13 +9,13 @@ import pandas as pd
 import json
 
 
-def append_evaluation_metrics_to_csv(model_name, evaluation_metrics,dataset_name,best_params,fit_time, filename='model_evaluation_metrics.csv'):
+def append_evaluation_metrics_to_csv(model_name, evaluation_metrics,dataset_name,best_params,fit_time,predict_time, filename='model_evaluation_metrics.csv'):
     try:
         # Load existing CSV file
         df_metrics = pd.read_csv(filename)
     except FileNotFoundError:
         # If the file doesn't exist yet, create an empty DataFrame
-        df_metrics = pd.DataFrame(columns=['Model_Name','Dataset','Parameters','Accuracy', 'Precision', 'Recall', 'F1_Score', 'Fit_Time'])
+        df_metrics = pd.DataFrame(columns=['Model_Name','Dataset','Parameters','Accuracy', 'Precision', 'Recall', 'F1_Score', 'Fit_Time','Predict_time'])
 
     # Convert the best_params dictionary to a JSON string
     best_params_str = json.dumps(best_params)
@@ -23,8 +23,8 @@ def append_evaluation_metrics_to_csv(model_name, evaluation_metrics,dataset_name
 
     # Create a DataFrame with the new metrics
     new_row = pd.DataFrame([[model_name,dataset_name,best_params_str, evaluation_metrics['accuracy'], evaluation_metrics['precision'],
-                             evaluation_metrics['recall'], evaluation_metrics['f1_score'], fit_time]],
-                           columns=['Model_Name', 'Dataset','Parameters','Accuracy', 'Precision', 'Recall', 'F1_Score', 'Fit_Time'])
+                             evaluation_metrics['recall'], evaluation_metrics['f1_score'], fit_time, predict_time]],
+                           columns=['Model_Name', 'Dataset','Parameters','Accuracy', 'Precision', 'Recall', 'F1_Score', 'Fit_Time','Predict_time'])
 
     # Append the new row to the existing DataFrame
     df_metrics = pd.concat([df_metrics, new_row], ignore_index=True)
